e6b3015fc4be68e9051902808588bed3cb412b4e
Devesh Patel <deveshp530@gmail.com>
2022-02-01 16:37:54 -0800

    added launches model

--- server/app.js
+++ server/app.js
@@ -1,18 +1,28 @@
 const express = require("express");
+const path = require("path");
+const morgan = require("morgan");
 const compression = require("compression");
-const planetsRouter = require("./src/planets/planets.router");
+const planetsRouter = require("./src/routes/planets/planets.router");
+const launchesRouter = require("./src//routes/launches/launches.router");
 
 const cors = require("cors");
 
 const app = express();
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
 app.use(
   cors({
     origin: "http://localhost:3000",
   })
 );
+app.use(morgan("combined"));
 app.use(compression());
 app.use(planetsRouter);
+app.use(launchesRouter);
+
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
 module.exports = app;
---
+++ server/src/models/launches.model.js
@@ -0,0 +1,18 @@
+const launches = new Map();
+
+const launch = {
+  mission: "kepler expoloration x",
+  rocket: "expolorer is1",
+  launchDate: new Date("May 30, 2030"),
+  destination: "kepler-442 b",
+  flightNumber: 100,
+  customers: ["nasa", "ztm"],
+  upcoming: true,
+  success: true,
+};
+
+launches.set(launch.flightNumber, launch);
+
+module.exports = {
+  launches,
+};
